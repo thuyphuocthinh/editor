188812c4e5b5cb5cfdca0adcd256944b53b4d003
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2025-11-19 22:35:08 +0700

    feat: add initial HTML structure and configure Vite for build and deployment
    - Created a new index.html file with basic structure for the Mini Editor.
    - Updated package.json to include a deploy script using gh-pages.
    - Added gh-pages as a dev dependency.
    - Configured Vite to set the root directory and output directory for builds.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: ".", // thư mục gốc chứa index.html
+  build: {
+    outDir: "dist", // thư mục build ra
+    rollupOptions: {
+      input: "./index.html", // file HTML chính
+    },
+  },
   server: {
     port: 3000,
     open: true,
